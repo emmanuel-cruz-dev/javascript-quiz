f4398b01c2762c85990c7e9e863a9370e7cc79a5
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-14 12:47:23 -0300

    estilos en card de Game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -14,18 +14,21 @@ import { type Question as QuestionType } from "../types/types";
 
 const Question = ({ info }: { info: QuestionType }) => {
   return (
-    <Card variant="outlined" sx={{ bgcolor: "#222", p: 2, textAlign: "left" }}>
+    <Card
+      variant="outlined"
+      sx={{ bgcolor: "#222", p: 2, textAlign: "left", marginTop: 4 }}
+    >
       <Typography variant="h5">{info.question}</Typography>
 
       <SyntaxHighlighter language="javascript" style={gradientDark}>
         {info.code}
       </SyntaxHighlighter>
 
-      <List sx={{ bgcolor: "#333" }}>
+      <List sx={{ bgcolor: "#333" }} disablePadding>
         {info.answers.map((answer, index) => (
-          <ListItem key={index}>
+          <ListItem key={index} disablePadding divider>
             <ListItemButton>
-              <ListItemText primary={answer} />
+              <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
           </ListItem>
         ))}
