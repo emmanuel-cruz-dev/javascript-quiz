f4950d7afb8e62acac8c71115508c42504ef076e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-14 12:11:03 -0300

    agregando Game.tsx

---
+++ src/components/Game.tsx
@@ -0,0 +1,17 @@
+// import { IconButton, Stack } from "@mui/material";
+import { useQuestionsStore } from "../store/questions";
+
+const Question = ({ info }) => {};
+
+export const Game = () => {
+  const questions = useQuestionsStore((state) => state.questions);
+  const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
+
+  const questionInfo = questions[currentQuestion];
+
+  return (
+    <>
+      <Question />
+    </>
+  );
+};
