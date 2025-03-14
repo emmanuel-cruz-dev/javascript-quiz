b09fc4cea2fc41852ad04e56eecbd1d8cb602e2d
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-14 15:29:57 -0300

    hook useQuestionsData creado

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,22 +1,11 @@
-import { useQuestionsStore } from "../store/questions";
+import { useQuestionsData } from "../hooks/useQuestionsData";
 
 export function Footer() {
-  const questions = useQuestionsStore((state) => state.questions);
-
-  let correct = 0;
-  let incorrect = 0;
-  let unanswered = 0;
-
-  questions.forEach((question) => {
-    const { userSelectedAnswer, correctAnswer } = question;
-    if (userSelectedAnswer == null) unanswered++;
-    else if (userSelectedAnswer === correctAnswer) correct++;
-    else incorrect++;
-  });
+  const { correct, incorrect, unanswered } = useQuestionsData();
 
   return (
     <footer style={{ marginTop: "1rem" }}>
-      <strong>{`✅ ${correct} - ❌ ${incorrect} - ? ${unanswered}`}</strong>
+      <strong>{`✅ ${correct} correctas - ❌ ${incorrect} incorrectas - ? ${unanswered} sin responder`}</strong>
     </footer>
   );
 }
