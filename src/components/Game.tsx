8f67f7de0a5ff44f82cf3eb5bf0436e964a60e5f
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-14 13:36:14 -0300

    coloreando preguntas error y correct

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -12,6 +12,21 @@ import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { useQuestionsStore } from "../store/questions";
 import { type Question as QuestionType } from "../types/types";
 
+const getBackgroundColor = (info: QuestionType, index: number) => {
+  const { userSelectedAnswer, correctAnswer } = info;
+  // Usuario no ha seleccionado nada todavía
+  if (userSelectedAnswer == null) return "transparent";
+  // Si ya seleccionó pero la solución es incorrecta
+  if (index != correctAnswer && index != userSelectedAnswer)
+    return "transparent";
+  // Si esta es la solución correcta
+  if (index === correctAnswer) return "green";
+  // Si esta es la selección del usuario pero no es correcta
+  if (index === userSelectedAnswer) return "red";
+  // Si no es ninguna de las anteriores
+  return "transparent";
+};
+
 const Question = ({ info }: { info: QuestionType }) => {
   const selectAnswer = useQuestionsStore((state) => state.selectAnswer);
 
@@ -33,7 +48,10 @@ const Question = ({ info }: { info: QuestionType }) => {
       <List sx={{ bgcolor: "#333" }} disablePadding>
         {info.answers.map((answer, index) => (
           <ListItem key={index} disablePadding divider>
-            <ListItemButton onClick={createHandleClick(index)}>
+            <ListItemButton
+              onClick={createHandleClick(index)}
+              sx={{ backgroundColor: getBackgroundColor(info, index) }}
+            >
               <ListItemText primary={answer} sx={{ textAlign: "center" }} />
             </ListItemButton>
           </ListItem>
